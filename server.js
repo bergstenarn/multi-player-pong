3284ade44289634b3932f6c838298a3c5f8fec6d
bergstenarn <andersbergsten@yahoo.se>
2022-04-25 22:15:41 +0300

    310. Implementing the Game Logic: Ball

--- server.js
+++ server.js
@@ -26,4 +26,8 @@ io.on("connection", (socket) => {
   socket.on("paddleMove", (paddleData) => {
     socket.broadcast.emit("paddleMove", paddleData);
   });
+
+  socket.on("ballMove", (ballData) => {
+    socket.broadcast.emit("ballMove", ballData);
+  });
 });
