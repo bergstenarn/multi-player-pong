a1acd9e482ee118f7e155744438189b4941ee0e0
bergstenarn <andersbergsten@yahoo.se>
2022-04-25 21:34:53 +0300

    309. Implementing the Game Logic: Paddle

--- server.js
+++ server.js
@@ -13,11 +13,17 @@ console.log(`Listening on port ${PORT} ...`);
 let readyPlayerCount = 0;
 io.on("connection", (socket) => {
   console.log(`A user connected: ${socket.id}`);
+
   socket.on("ready", () => {
     console.log(`Player ready: ${socket.id}`);
     readyPlayerCount++;
     if (readyPlayerCount === 2) {
+      console.log("Starting game ...");
       io.emit("startGame", socket.id);
     }
+  });
+
+  socket.on("paddleMove", (paddleData) => {
+    socket.broadcast.emit("paddleMove", paddleData);
   });
 });
